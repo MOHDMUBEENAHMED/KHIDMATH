c8168048391de260dfa51945fefcbf548dcbe933
MOHDMUBEENAHMED <mamubeen1186@gmail.com>
2024-09-15 00:38:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function clickChange(){
+
+    let select2 = document.getElementById("select2")
+    if(select2.value == ""){
+        alert(" select choose..  options   ")
+    }else{
+         window.location.href=("index2-input.html")
+    }
+}
+
+function clickChange2(){
+    window.location.href="index6-end.html";
+  }
+
+function clickChange3(){
+    window.location.href="index.html";
+  }
+
+  
